c691f1f9904595bd485f5159f53fbedf8e29c1e8
derkalaender <git@derkalaender.de>
2024-07-07 15:49:51 +0200

    fix: log spam

--- meshnow/src/receive/receiver.cpp
+++ meshnow/src/receive/receiver.cpp
@@ -7,12 +7,13 @@
 
 namespace meshnow::receive {
 
+namespace {
+constexpr auto TAG = CREATE_TAG("Receiver");
+}
+
 void Receiver::receiveCallback(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int data_len) {
     // convert raw data pointer into buffer (vector) for deserialization TODO avoid this
     std::vector<uint8_t> buffer(data, data + data_len);
-
-    //    ESP_LOGI("TEST", "RECEIVED DATA");
-    //    ESP_LOG_BUFFER_HEXDUMP("TEST", buffer.data(), buffer.size(), ESP_LOG_INFO);
 
     // deserialize
     auto packet = packets::deserialize(buffer);
@@ -20,7 +21,7 @@ void Receiver::receiveCallback(const esp_now_recv_info_t *esp_now_info, const ui
     // if deserialization failed, ignore
     // could happen because of interference with connecting to a router
     if (!packet) {
-        ESP_LOGW("TAG", "Failed to deserialize packet!");
+        ESP_LOGV(TAG, "Failed to deserialize packet!");
         return;
     }
 
